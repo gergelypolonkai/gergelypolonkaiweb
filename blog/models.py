2eecd9caa8ad311bd7c7f750c251f2e1d45eee21
Gergely Polonkai <gergely@polonkai.eu>
2013-10-02 13:24:04 +0200

    Fixup for blog.Post
    A little automation for first time saving: set created_at and slug fields

--- blog/models.py
+++ blog/models.py
@@ -4,12 +4,18 @@ from taggit.managers import TaggableManager
 
 class Post(models.Model):
     user = models.ForeignKey(User)
-    created_at = models.DateTimeField()
+    created_at = models.DateTimeField(auto_now_add=True)
     title = models.CharField(max_length = 100)
-    slug = models.CharField(max_length = 100)
+    slug = models.SlugField(editable=False, max_length = 100)
     content = models.TextField()
     draft = models.BooleanField()
     tags = TaggableManager()
 
     def __unicode__(self):
         return self.title
+
+    def save(self, *args, **kwargs):
+        if not self.id:
+            self.slug = slugify(self.title)
+
+        super(Post, self).save(*args, **kwargs)
