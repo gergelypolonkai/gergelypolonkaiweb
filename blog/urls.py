87d7e16b0e7a2ee622365caf2c51673510ce0f60
Gergely Polonkai (W00d5t0ck) <gergely@polonkai.eu>
2013-10-14 12:12:50 +0200

    Add pagination support for blog listing

--- blog/urls.py
+++ blog/urls.py
@@ -2,8 +2,10 @@ from django.conf.urls import patterns, url
 from blog import views
 
 urlpatterns = patterns('',
-    url(r'^$',                                                          views.index,   name='index'),
-    url(r'^feed$',                                                      views.feed,    name='feed'),
-    url(r'^tag/(?P<tag>.*)$',                                           views.taglist, name='taglist'),
-    url(r'^(?P<year>\d+)/(?P<month>\d+)/(?P<day>\d+)/(?P<slug>[^/]+)$', views.read,    name='read'),
+    url(r'^$',                                                          views.index,     name='index'),
+    url(r'^page/(?P<page>\d+)$',                                        views.indexpage, name='indexpage'),
+    url(r'^tag/(?P<tag>[^/]+)$',                                        views.taglist,   name='taglist'),
+    url(r'^tag/(?P<tag>[^/]+)/page/(?P<page>\d+)$',                     views.tagpage,   name='tagpage'),
+    url(r'^(?P<year>\d+)/(?P<month>\d+)/(?P<day>\d+)/(?P<slug>[^/]+)$', views.read,      name='read'),
+    url(r'^feed$',                                                      views.feed,      name='feed'),
 )
--- blog/views.py
+++ blog/views.py
@@ -1,22 +1,46 @@
 import datetime
 from django.shortcuts import render, get_object_or_404
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from blog.models import Post
 
 def mainpage(request):
-    last_posts = Post.objects.filter(draft=False).order_by('-created_at')[:5]
+    last_posts = Post.objects.filter(draft = False).order_by('-created_at')[:5]
     return render(request, 'blog/listing.html', {'posts': last_posts})
 
+def listing(request, tag, page):
+    if (tag == None):
+        post_list = Post.objects.filter(draft = False)
+        view = "index"
+    else:
+        post_list = Post.objects.filter(tags__slug = tag, draft = False)
+        view = "tagage"
+
+    paginator = Paginator(post_list.order_by('-created_at'), 10)
+
+    try:
+        posts = paginator.page(page)
+    except PageNotAnInteger:
+        posts = paginator.page(1)
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)
+
+    return render(request, 'blog/listing.html', { 'posts': posts, 'view': "blog:" + view })
+
 def index(request):
-    last_posts = Post.objects.filter(draft=False).order_by('-created_at')
-    return render(request, 'blog/listing.html', {'posts': last_posts})
+    return listing(request, None, 1)
+
+def indexpage(request, page):
+    return listing(request, None, page)
+
+def taglist(request, tag):
+    return listing(request, tag, 1)
+
+def tagpage(request, tag, page):
+    return listing(request, tag, page)
 
 def read(request, year, month, day, slug):
     post = get_object_or_404(Post, created_at__year=int(year), created_at__month=int(month), created_at__day=int(day), slug=slug, draft=False);
     return render(request, 'blog/view.html', {'post': post})
-
-def taglist(request, tag):
-    posts = Post.objects.filter(tags__slug=tag, draft=False)
-    return render(request, 'blog/listing.html', {'posts': posts})
 
 def feed(request):
     return render(request, 'blog/feed.xml', {})
