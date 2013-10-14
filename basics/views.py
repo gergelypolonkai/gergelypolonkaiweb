d8f2746abc6e346d4855b57aca352deefb096117
Gergely Polonkai (W00d5t0ck) <gergely@polonkai.eu>
2013-10-14 18:07:15 +0200

    Add HTML resume

--- basics/views.py
+++ basics/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
+from django.contrib.sites.models import get_current_site
 
 def resume(request):
-    return render(request, 'resume.html', {})
+    return render(request, 'basics/resume.html', { 'site': get_current_site(request) })
 
 def about(request):
     return render(request, 'basics/about.html', {})
