a5b6e901830123909cafab93f32b8589d16a0cc1
Gergely Polonkai <gergely@polonkai.eu>
2013-10-21 02:36:19 +0200

    Add insert_code_chunks template filter
    This basic version actually removes them. Addition to be coming later

--- blog/templatetags/code_chunks.py
+++ blog/templatetags/code_chunks.py
@@ -1,11 +1,12 @@
 from django import template
 from django.template.defaultfilters import stringfilter
 from django.utils.safestring import mark_safe
-import logging
+import logging, re
 from pygments import highlight
 from pygments.lexers import get_lexer_by_name
 from pygments.formatters import HtmlFormatter
 from gergelypolonkaiweb.solarized_dark import SolarizedDarkStyle
+from blog.models import CodeChunk
 
 register = template.Library()
 
@@ -26,6 +27,48 @@ def hilite(language, code):
     lexer = get_lexer_by_name(language)
     formatter = CodeFormatter(style = SolarizedDarkStyle, linenos = False, cssclass = language + " code", noclasses = True)
     return highlight(code, lexer, formatter)
+
+@register.filter(needs_autoescape=True)
+@stringfilter
+def insert_code_chunks(value, autoescape=None):
+    p = re.compile('\[\$ code:(?P<lang>[^:]+):(?P<slug>[^ ]+) \$\]')
+    i = p.finditer(value)
+    diff = 0
+
+    for match in i:
+        end, start = match.span()
+        oldlen = start - end
+        start += diff
+        end += diff
+        try:
+            chunk = CodeChunk.objects.get(language = match.group('lang'), slug = match.group('slug'))
+            # TODO: This is an ugly hack, as it includes template logic in code. BAD!
+            newstr = "<div class=\"code-chunk\">" + hilite(match.group('lang'), chunk.content) + "</div>"
+        except CodeChunk.DoesNotExist:
+            newstr = ""
+
+        newlen = len(newstr)
+        value = value[:end] + newstr + value[start:]
+        diff += newlen - oldlen
+
+    p = re.compile('\[\$ code:(?P<lang>[^:]+):(?P<code>.+?) \$\]', re.DOTALL)
+    i = p.finditer(value)
+    diff = 0
+
+    for match in i:
+        end, start = match.span()
+        oldlen = start - end
+        start += diff
+        end += diff
+
+        # TODO: This is an ugly hack, as it includes template logic in code. BAD!
+        newstr = "<div class=\"code-chunk\">" + hilite(match.group('lang'), match.group('code')) + "</div>"
+
+        newlen = len(newstr)
+        value = value[:end] + newstr + value[start:]
+        diff += newlen - oldlen
+
+    return mark_safe(value)
 
 @register.filter(needs_autoescape=True)
 @stringfilter
