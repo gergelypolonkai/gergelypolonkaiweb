6c2b7873c297ae4594ac1b68399b34681320124c
Gergely Polonkai <gergely@polonkai.eu>
2014-04-21 15:46:14 +0200

    Refine resume text and formatting

--- basics/urls.py
+++ basics/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import patterns, url
 from basics import views
 
 urlpatterns = patterns('',
-    url(r'^resume',     views.resume,     name='resume'),
-    url(r'^about',      views.about,      name='about'),
-    url(r'^disclaimer', views.disclaimer, name='disclaimer'),
+    url(r'^resume$',     views.resume,     name = 'resume'),
+    url(r'^about$',      views.about,      name = 'about'),
+    url(r'^disclaimer$', views.disclaimer, name = 'disclaimer'),
 )
