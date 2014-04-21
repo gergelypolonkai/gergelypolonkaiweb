4274455957c16c4df0ae3587c4c77ccb4f120ee0
Gergely Polonkai <gergely@polonkai.eu>
2014-04-21 10:53:53 +0200

    Add date listing function

--- blog/views.py
+++ blog/views.py
@@ -7,14 +7,27 @@ def mainpage(request):
     last_posts = Post.objects.filter(draft = False).order_by('-created_at')[:5]
     return render(request, 'blog/listing.html', {'posts': last_posts})
 
-def listing(request, tag, page):
+def listing(request, tag, year, month, day, page):
+    kwargs = {}
+    kwargs['draft'] = False
+
+    view = index
     if (tag == None):
-        post_list = Post.objects.filter(draft = False)
         view = "index"
     else:
-        post_list = Post.objects.filter(tags__slug = tag, draft = False)
-        view = "tag"
+        kwargs['tags__slug'] = tag
+        view = "tagpage"
 
+    if (year != None):
+        kwargs['created_at__year'] = year
+
+        if (month != None):
+            kwargs['created_at__month'] = month
+
+            if (day != None):
+                kwargs['created_at__day'] = day
+
+    post_list = Post.objects.filter(**kwargs)
     paginator = Paginator(post_list.order_by('-created_at'), 10)
 
     try:
@@ -27,16 +40,22 @@ def listing(request, tag, page):
     return render(request, 'blog/listing.html', { 'posts': posts, 'view': "blog:" + view })
 
 def index(request):
-    return listing(request, None, 1)
+    return listing(request, None, None, None, None, 1)
 
 def indexpage(request, page):
-    return listing(request, None, page)
+    return listing(request, None, None, None, None, page)
 
 def taglist(request, tag):
-    return listing(request, tag, 1)
+    return listing(request, tag, None, None, None, 1)
 
 def tagpage(request, tag, page):
-    return listing(request, tag, page)
+    return listing(request, tag, None, None, None, page)
+
+def datelist(request, year, month, day):
+    return listing(request, None, year, month, day, 1)
+
+def datepage(request, year, month, day, page):
+    return listing(request, None, year, month, day, page)
 
 def read(request, year, month, day, slug):
     post = get_object_or_404(Post, created_at__year=int(year), created_at__month=int(month), created_at__day=int(day), slug=slug, draft=False)
