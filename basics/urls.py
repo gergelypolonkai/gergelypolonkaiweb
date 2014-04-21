cf08e3e0e4f77751699edb97ae95ca415ae7cd26
Gergely Polonkai <gergely@polonkai.eu>
2014-04-21 15:46:55 +0200

    Generate PDF version of the resume

--- basics/urls.py
+++ basics/urls.py
@@ -3,6 +3,7 @@ from basics import views
 
 urlpatterns = patterns('',
     url(r'^resume$',     views.resume,     name = 'resume'),
+    url(r'^resume.pdf$', views.resumepdf,  name = 'resumepdf'),
     url(r'^about$',      views.about,      name = 'about'),
     url(r'^disclaimer$', views.disclaimer, name = 'disclaimer'),
 )
--- basics/views.py
+++ basics/views.py
@@ -1,9 +1,26 @@
 from django.shortcuts import render
 from django.contrib.sites.models import get_current_site
 from django.http import HttpResponse
+from django.template.loader import render_to_string
+from StringIO import StringIO
+from xhtml2pdf import pisa
 
 def googlevalidator(request):
     return HttpResponse('')
+
+def resumepdf(request):
+    body = render_to_string('basics/resume.html', { 'site': get_current_site(request), 'pdf': True })
+    dst = StringIO()
+    pdf = pisa.CreatePDF(body, dst)
+    pdf_data = dst.getvalue()
+    dst.close()
+
+    if not pdf.err:
+        return HttpResponse(pdf_data, mimetype = 'application/pdf')
+
+    return HttpResponse('We had some errors<pre>%s</pre>' % escape(html))
+
+    return render(request, 'basics/resume.html', { 'site': get_current_site(request), 'pdf': True })
 
 def resume(request):
     return render(request, 'basics/resume.html', { 'site': get_current_site(request) })
