ee02fab3a3e5f990cb6723b92fb85039531fc063
Gergely Polonkai <gergely@polonkai.eu>
2014-06-14 13:00:32 +0000

    Add database options for correct table encoding

--- gergelypolonkaiweb/settings.py
+++ gergelypolonkaiweb/settings.py
@@ -21,6 +21,7 @@ DATABASES = {
         'PASSWORD': 'the8dooM',
         'HOST': '',                            # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
         'PORT': '',                            # Set to empty string for default.
+        'OPTIONS': {'init_command': 'SET storage_engine=INNODB,character_set_connection=utf8,collation_connection=utf8_unicode_ci'},
     }
 }
 
