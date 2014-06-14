0073c1521c9d1b2bd4bc073ca8c497fa0e8cc35f
Gergely Polonkai <gergely@polonkai.eu>
2014-06-14 13:00:00 +0000

    Fix TEMPLATE_DIRS in settings.py to be a correct tuple
    Django >1.5 complains about it to be deprecated

--- gergelypolonkaiweb/settings.py
+++ gergelypolonkaiweb/settings.py
@@ -116,7 +116,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    SITE_ROOT + os.path.sep + "templates"
+    SITE_ROOT + os.path.sep + "templates",
 )
 
 INSTALLED_APPS = (
