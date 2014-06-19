1b8bbb55fe63183db870db1d71dc01fd9d672c5f
Gergely Polonkai <gergely@polonkai.eu>
2014-06-19 08:49:15 +0000

    Fix ViewName middleware after accidentally changing . to +

--- gergelypolonkaiweb/middleware/viewname.py
+++ gergelypolonkaiweb/middleware/viewname.py
@@ -1,5 +1,4 @@
 class ViewNameMiddleware(object):
     def process_view(self, request, view_func, view_args, view_kwargs):
-        request.view_name = "."
-                + join((view_func.__module__, view_func.__name__))
+        request.view_name = ".".join((view_func.__module__, view_func.__name__))
 
