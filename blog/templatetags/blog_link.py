a7c649d5c17d20680ff04be9d5100548d8cda2a3
Gergely Polonkai <gergely@polonkai.eu>
2014-06-22 15:11:40 +0000

    Add get_post_relative_link template tag to generate relative links to blog posts

--- blog/templatetags/blog_link.py
+++ blog/templatetags/blog_link.py
@@ -1,7 +1,14 @@
 from django import template
 from django.core.urlresolvers import reverse
+from django.conf import settings
+import pytz
 
 register = template.Library()
+
+@register.simple_tag
+def get_post_relative_link(post):
+    post_date = post.created_at.astimezone(pytz.timezone(settings.TIME_ZONE))
+    return reverse('blog:read', args=(post_date.year, post_date.month, post_date.day, post.slug))
 
 @register.simple_tag(takes_context = True)
 def get_post_link(context, post):
